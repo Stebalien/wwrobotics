ca888560041812325f266526bb4bd42d926d033f
jessebwr <jessebwr@94d52c14-dd1b-11dd-b4f8-6d137782c98b>
2009-01-10 22:47:02 +0000

    Added comments

--- testCode.cpp
+++ testCode.cpp
@@ -7,15 +7,16 @@ class Robotdemo : public SimpleRobot
 {
 	RobotDemo (void)
 	{
-		GetWatchdog().SetEnabled(false);
+		GetWatchdog().SetEnabled(false); // disables watchdog
 	}
 	Void Autonomous (void)
 	{
 		for (int i = 0; i<4; i++)
-		drivetrain.Drive(.5, 0.0);
-		wait(2000);
-		drivetrain.Drive(0.0, 0.75);
+		drivetrain.Drive(.5, 0.0); // after Driver the .5 means 50% speed and the 0.0 is a 0% turn
+		wait(2000); // measured in miliseconds, so 2 second wait
+		drivetrain.Drive(0.0, 0.75); // 0%  power/speed and 75% turn
 		wait(750);
+		drivetrain.Drive(0.0, 0.0); // stops movement (I don't know if this is needed but it was in the guide with essentially this comment)
 	}
 	Void OperatorControl (void)
 	{
@@ -26,4 +27,10 @@ class Robotdemo : public SimpleRobot
 	}
 };
 START_ROBOT_CLASS (RobotDemo);
+
+
+
+
+
+
 
