fabeaf44b72243da363ed9be5cbd24c2f0c1eb32
jigajigajoo <jigajigajoo@94d52c14-dd1b-11dd-b4f8-6d137782c98b>
2009-01-17 22:55:29 +0000

    Added treadSpeed constant and support for tread Motors @ PWM 3&4. ArcadeMode driving (1-stick).

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -8,18 +8,23 @@
  */ 
 class RobotDemo : public SimpleRobot
 {
+	const static float treadSpeed = 0.5; // controls speed of treads
 	RobotDrive myRobot; // robot drive system
 	Joystick stick1; //  joystick 1
 	Joystick stick2; // joystick 2 (two total joysticks)
-	
+	Jaguar treadsController1; // Jaguar speed controller for treads motor 1
+	Jaguar treadsController2; // Jaguar speed controller for treads motor
 public:
 	RobotDemo(void):
 		myRobot(1, 2),	// these must be initialized in the same order
 		stick1(1),		// as they are declared above.
-		stick2(2)
+		stick2(2),
+		treadsController1(3), // controllers in PWM ports 3 and 4
+		treadsController2(4)
 		{
 		GetWatchdog().SetExpiration(300);
-	}
+		GetWatchdog().SetEnabled(false); //disable watchdog for testing reasons
+		}
 
 	/**
 	 * Drive left & right motors for 2 seconds then stop
@@ -27,25 +32,34 @@ public:
 	void Autonomous(void)
 	{
 		GetWatchdog().SetEnabled(false);
-		myRobot.Drive(0.3, 0.0); 	// drive forwards half speed
-		//Wait(2.0); 				//    for 2 seconds
-		//myRobot.Drive(0.0, 0.0);	// stop robot
+		myRobot.Drive(0.5, 0.0); 	// drive forwards half speed
+		Wait(2.0); 				//    for 2 seconds
+		myRobot.Drive(-0.5, 0.0);	// reverse robot
 	}
 
 	/**
-	 * Runs the motors with arcade steering. 
+	 * Runs the motors with tank steering. 
 	 */
 	void OperatorControl(void)
 	{
-		GetWatchdog().SetEnabled(true);
+		//GetWatchdog().SetEnabled(true);
 		while (IsOperatorControl())
 		{
-			GetWatchdog().Feed();
+			//GetWatchdog().Feed();
 			if(stick1.GetTrigger()) {
 				myRobot.Drive(0.0, 0.0);
 			}
-			else
-				myRobot.TankDrive(stick1, stick2); // drive with arcade style (use right stick)
+			else {
+				myRobot.ArcadeDrive(stick1); // drive with arcade style (use right stick)
+				if(stick2.GetTop()) {
+					treadsController1.Set(treadSpeed);
+					treadsController2.Set(treadSpeed);
+				}
+				else if(stick1.GetTop()) {
+					treadsController1.Set(-treadSpeed);
+					treadsController2.Set(-treadSpeed);
+				}
+			}
 		}
 	}
 };
